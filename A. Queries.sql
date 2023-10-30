951ec3765b91f25e1b359e4601917c8715e28e3c
tomidatabricks <tomidatabricks>
2023-10-30 02:38:08 +0000

    made the queries more generic

--- A. Queries.sql
+++ A. Queries.sql
@@ -9,15 +9,57 @@ USE DATABASE main;
 
 -- COMMAND ----------
 
--- MAGIC %md ##Row Counts an last updated by for each table
+-- MAGIC %md ##Volumes
 -- MAGIC
--- MAGIC TODO rewrite in pyspark, make it dynamic, look up the tables that exist in the database and create the report for those tables
--- MAGIC TODO add info about models and feature stores and volumes in the same database as well
 
 -- COMMAND ----------
 
-          SELECT "raw_telemetry" AS name, count(1), NULL AS t FROM raw_telemetry
-UNION ALL SELECT "clean_telemetry" AS name, count(1), max(created_at) AS t FROM clean_telemetry
-UNION ALL SELECT "clean_telemetry_features" AS name, count(1), max(created_at) AS t FROM telemetry_features
-UNION ALL SELECT "telemetry_labels_and_features" AS name, count(1), max(created_at) AS t FROM telemetry_labels_and_features
-ORDER BY t ASC
+DESCRIBE VOLUME telemetry_analytics_cat.main.landing;
+
+-- COMMAND ----------
+
+-- MAGIC %md ##Tables
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC
+-- MAGIC tables_df = spark.sql("SELECT * FROM telemetry_analytics_cat.information_schema.tables WHERE table_schema='main'");
+-- MAGIC
+-- MAGIC display(tables_df)
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC
+-- MAGIC sqls = [f"""
+-- MAGIC   SELECT "{table['table_name']}" AS name
+-- MAGIC    ,count(1) AS num_rows
+-- MAGIC    ,"{table['last_altered']}" AS last_altered 
+-- MAGIC   FROM telemetry_analytics_cat.main.{table['table_name']}""" for table in  tables_df.collect() ]
+-- MAGIC
+-- MAGIC query = " UNION ALL\n".join(sqls)+"\n ORDER BY last_altered ASC"
+-- MAGIC
+-- MAGIC display(spark.sql(query))
+
+-- COMMAND ----------
+
+-- MAGIC %md ##Models
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC
+-- MAGIC # I will need an MLRuntime for that
+-- MAGIC # import mlflow
+-- MAGIC # mlflow.set_registry_uri("databricks-uc")
+-- MAGIC
+-- MAGIC #client=MlflowClient()
+-- MAGIC #client.search_registered_models()
+-- MAGIC
+
+-- COMMAND ----------
+
+
